4a7a8c422ea08370f27c04e9779f8f5dbc84bf25
krbn96 <jmakakb9313@gmail.com>
2026-09-13 16:55:24 +0900

    Initial Spring Boot project setup

---
+++ src/main/java/com/fighterhub/FighterHubApplication.java
@@ -0,0 +1,13 @@
+package com.fighterhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FighterHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FighterHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fighterhub/FighterHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fighterhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FighterHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
